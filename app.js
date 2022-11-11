7f59a5376b8b26cc813bfd246d9d30efc2bacc65
rehan22113 <raorehanweb@gmail.com>
2022-11-12 00:06:18 +0500

    first commit
    exercise problem

---
+++ app.js
@@ -0,0 +1,42 @@
+// let names:string = "Hello world!"
+// interface model
+// interface heloo{
+//     name:string,
+//     email:string
+// }
+//object in typescript
+// let b:heloo ={
+//     name:"2",
+//     email:"raorehanweb@gmail.com"
+// }
+// b.add = "hello"
+// console.log(b.email)
+//Array concept
+// let arr:(string|number)[] = [1,2,"4",5]
+// console.log(arr[2])
+//Function
+// function Add():number{
+//     return 2
+// }
+// console.log(Add())
+// function Adds(a:string,b:number):number{
+//     let t = parseInt(a)
+//     return t+b
+// }
+// console.log(Adds("2",3))
+//Make a Infinite Recursion Function
+// function Recursive(a:number):any{
+//         return function (b:number):any{
+//             if(b){
+//                 return Recursive(a+b)
+//             }
+//             else{
+//                 return a
+//             }
+//          }
+// }
+// console.log(Recursive(5)(2)(4)(3)())
+// enum Data{
+//     a=10,b,c,d,e
+// }
+// console.log(Data.e)
